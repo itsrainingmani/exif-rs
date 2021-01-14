d08001a963f50c21889c7077157ce37ad6d9d1ff
Manikandan Sundararajan <mani@hey.com>
2021-01-14 12:08:51 -0600

    Add image and test function to read bytes from image

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_img_file_read() {
+        let bytes: Vec<u8> = fs::read("DSCF1197.JPG").unwrap();
+        println!("{:#?}", bytes.len());
     }
 }
