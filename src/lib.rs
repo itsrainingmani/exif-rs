f0c5f31f0e84a0e4ec32eaecddbba4c29148a186
Manikandan Sundararajan <mani@hey.com>
2021-01-16 23:18:37 -0500

    Add test to convert byte vec to a hex string vec

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,22 @@ mod tests {
         println!("{:#?}", bytes.len());
 
         // Format the vector as hexadecimal integers with leading zeros
-        println!("{:#04X?}", bytes);
+        // println!("{:#04X?}", bytes);
+    }
+
+    #[test]
+    fn test_find_exif_marker() {
+        let bytes: Vec<u8> = fs::read("DSCF1197.JPG").unwrap();
+
+        println!("{:#?} {:#?}", bytes.get(0).unwrap(), bytes.get(1).unwrap());
+
+        // Convert vector of u8s to vector of hex strings
+        let hex_bytes: Vec<String> = bytes.iter().map(|b| format!("{:X}", b)).collect();
+
+        println!(
+            "{:#?} {:#?}",
+            hex_bytes.get(0).unwrap(),
+            hex_bytes.get(1).unwrap()
+        );
     }
 }
