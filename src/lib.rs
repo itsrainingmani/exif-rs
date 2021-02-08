c6b839f4f0fe553cc3907f0953609be7938801c6
Manikandan Sundararajan <mani@hey.com>
2021-02-08 18:34:29 -0500

    Move the exif processing code into an impl block for ExifVec

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 pub mod exif {
+    use std::fs;
+    use std::path::Path;
+
     fn convert_to_hex(v: &Vec<u8>, start: usize, end: usize) -> Result<String, &'static str> {
         Ok(v.get(start..end)
             .unwrap()
@@ -11,6 +14,7 @@ pub mod exif {
     pub type ExifVec = Vec<u8>;
     pub trait Exif {
         fn convert_to_hex(&self, start: usize, end: usize) -> Result<String, &'static str>;
+        fn process_image(&self) -> Result<(), &'static str>;
     }
 
     impl Exif for ExifVec {
@@ -23,11 +27,59 @@ pub mod exif {
                 .collect::<Vec<String>>()
                 .join(""))
         }
+
+        fn process_image(&self) -> Result<(), &'static str> {
+            let mut is_app1_marker = false;
+
+            if let &[255, 216] = self.get(0..2).expect("Couldn't get byte slice at index") {
+                println!("SOI Marker");
+            } else {
+                println!("SOI Marker FFD8 not found");
+                std::process::exit(-1);
+            }
+
+            if let &[255, 225] = self.get(2..4).expect("Couldn't get byte slice at index") {
+                println!("APP1 Marker");
+                is_app1_marker = true;
+            } else {
+                println!("APP1 Marker FFE1 not found");
+                std::process::exit(-1);
+            }
+
+            if is_app1_marker {
+                println!("Within APP1 Marker section");
+
+                // Get the size of the APP1 segment
+                let app1_size_hex =
+                    format!("{:02X}{:02X}", self.get(4).unwrap(), self.get(5).unwrap());
+
+                // This converts a Hex String (Base 16) into a usize
+                // println!("{:#?}", usize::from_str_radix("FFBC", 16));
+                // Deducting 2 to count for the size of the marker itself
+                let app1_data_size = usize::from_str_radix(app1_size_hex.as_str(), 16).unwrap() - 2;
+                let app1_data_end = app1_data_size + 6;
+
+                println!("{:#?} {:#?}", app1_size_hex, app1_data_size);
+
+                let app1_bytes: ExifVec = Vec::from(self.get(6..app1_data_end).unwrap());
+                println!("Size of App1 Bytes Vector - {:#?}", app1_bytes.len());
+
+                // Check if the Exif header is present - 45786969 0000
+                // This translates to Exif0000
+
+                let exif_header = app1_bytes.convert_to_hex(0, 6).unwrap();
+                if exif_header == String::from("457869660000") {
+                    println!("Exif Header is present");
+                }
+            }
+
+            Ok(())
+        }
     }
 
     #[cfg(test)]
     mod tests {
-        use super::{convert_to_hex, ExifVec};
+        use super::{Exif, ExifVec};
         use std::fs;
         use std::path::Path;
 
@@ -142,13 +194,13 @@ pub mod exif {
 
                 println!("{:#?} {:#?}", app1_size_hex, app1_data_size);
 
-                let app1_bytes: Vec<u8> = Vec::from(img_bytes.get(6..app1_data_end).unwrap());
+                let app1_bytes: ExifVec = Vec::from(img_bytes.get(6..app1_data_end).unwrap());
                 println!("Size of App1 Bytes Vector - {:#?}", app1_bytes.len());
 
                 // Check if the Exif header is present - 45786969 0000
                 // This translates to Exif0000
 
-                let exif_header = convert_to_hex(&app1_bytes, 0, 6).unwrap();
+                let exif_header = app1_bytes.convert_to_hex(0, 6).unwrap();
                 if exif_header == String::from("457869660000") {
                     println!("Exif Header is present");
                 }
