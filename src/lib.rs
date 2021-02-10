4143f8b101dbbbf3046e4dd9d72c921fc1743009
Manikandan Sundararajan <mani@hey.com>
2021-02-09 22:31:41 -0500

    Remove code under test_maintain_state since it's now in a separate function

--- src/lib.rs
+++ src/lib.rs
@@ -143,56 +143,11 @@ pub mod exif {
         }
 
         #[test]
-        fn test_maintain_state() {
+        fn test_process_image() {
             let img_path = Path::new("DSCF1197.JPG");
-            let img_bytes = fs::read(img_path).unwrap();
+            let img_bytes: ExifVec = fs::read(img_path).unwrap();
 
-            let mut is_app1_marker = false;
-
-            if let &[255, 216] = img_bytes.get(0..2).unwrap() {
-                println!("SOI Marker");
-            } else {
-                println!("SOI Marker FFD8 not found");
-                std::process::exit(-1);
-            }
-
-            if let &[255, 225] = img_bytes.get(2..4).unwrap() {
-                println!("APP1 Marker");
-                is_app1_marker = true;
-            } else {
-                println!("APP1 Marker FFE1 not found");
-                std::process::exit(-1);
-            }
-
-            if is_app1_marker {
-                println!("Within APP1 Marker section");
-
-                // Get the size of the APP1 segment
-                let app1_size_hex = format!(
-                    "{:02X}{:02X}",
-                    img_bytes.get(4).unwrap(),
-                    img_bytes.get(5).unwrap()
-                );
-
-                // This converts a Hex String (Base 16) into a usize
-                // println!("{:#?}", usize::from_str_radix("FFBC", 16));
-                // Deducting 2 to count for the size of the marker itself
-                let app1_data_size = usize::from_str_radix(app1_size_hex.as_str(), 16).unwrap() - 2;
-                let app1_data_end = app1_data_size + 6;
-
-                println!("{:#?} {:#?}", app1_size_hex, app1_data_size);
-
-                let app1_bytes: ExifVec = Vec::from(img_bytes.get(6..app1_data_end).unwrap());
-                println!("Size of App1 Bytes Vector - {:#?}", app1_bytes.len());
-
-                // Check if the Exif header is present - 45786969 0000
-                // This translates to Exif0000
-
-                let exif_header = app1_bytes.convert_to_hex(0, 6).unwrap();
-                if exif_header == String::from("457869660000") {
-                    println!("Exif Header is present");
-                }
-            }
+            assert!(img_bytes.process_image().is_ok())
         }
     }
 }
